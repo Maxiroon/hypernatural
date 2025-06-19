c36cf7faa251200ad4def238119d3828c73653a7
Maxiroon <contactmaxiroon@gmail.com>
2025-06-19 06:21:41 -0500

    Initial commit x2

---
+++ JS/template.js
@@ -0,0 +1,17 @@
+document.getElementById("header").innerHTML = `
+    <div id="topnav">
+        <a href="/">Home</a>
+        <a href="/releases">Music</a>
+        <a href="/about">About</a>
+        <a href="https://cpi.maxiroon.xyz/">Community Picked Islands</a>
+        <a class="ethereal" href="/releases/msm/hypernatural">Hypernaturals</a>
+    </div>`;
+
+document.getElementById("footer").innerHTML = `
+    <br>
+    <center>
+        <img src="/media/textred.png" alt="MAXIROON" width="150px">
+        <br>
+        <a href="/">Home Page</a>
+    </center>
+    <br>`;
